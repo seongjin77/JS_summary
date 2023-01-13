7cae4e31e5937f922149c802663f65522e745586
seongjin <kssj9056@gmail.com>
2023-01-13 23:43:49 +0900

    자스정리

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-console.log(
-  typeof (1/'a'), isNaN(1/'a'), Number.isNaN(1/'a')
-); // NaN값인 경우
+let baby1 = '홍길동';
+let baby2; // 아직 이름을 짓지 못함
+
+const nameTag1 = baby1 ?? '1번 아기';
+const nameTag2 = baby2 ?? '2번 아기';
+
+console.log(nameTag1, nameTag2);
