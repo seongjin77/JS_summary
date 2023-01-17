20077023c9eb02898dffb7554c4bfa5cf934d1ff
seongjin <kssj9056@gmail.com>
2023-01-17 23:53:36 +0900

    event loop

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-let baby1 = '홍길동';
-let baby2; // 아직 이름을 짓지 못함
 
-const nameTag1 = baby1 ?? '1번 아기';
-const nameTag2 = baby2 ?? '2번 아기';
 
-console.log(nameTag1, nameTag2);
+var 함수 = () => {
+  console.log('11');
+}
+//console.log(함수());
